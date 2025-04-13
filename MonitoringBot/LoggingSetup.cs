ad8c2f67c6e46c3d5795be47b7b5355dddb2b734
Alexander Tyushin <atyushin@integrotechnologies.ru>
2025-04-13 15:44:49 +0300

    try fix logs

--- MonitoringBot/LoggingSetup.cs
+++ MonitoringBot/LoggingSetup.cs
@@ -5,16 +5,28 @@ public class LoggingSetup
 {
     public static void SetupLogging()
     {
+        // Получаем абсолютный путь к директории логов
+        string logDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs");
+        string logFilePath = Path.Combine(logDirectory, "log.txt");
+
+        // Создаём директорию, если её нет
+        if (!Directory.Exists(logDirectory))
+        {
+            Directory.CreateDirectory(logDirectory);
+        }
+
         Log.Logger = new LoggerConfiguration()
         .MinimumLevel.Debug()
         .WriteTo.Console()
             .WriteTo.File(
-                path: "Logs/log.txt",
+                path: logFilePath,
                 rollingInterval: RollingInterval.Day,
                 fileSizeLimitBytes: 50 * 1024 * 1024, // 10 МБ
                 rollOnFileSizeLimit: true,
                 retainedFileCountLimit: 7 // Хранить максимум 7 файлов, каждый заданное количество мегабайт
             )
         .CreateLogger();
+
+         Log.Warning($"Логи будут записываться в файл: {logFilePath}");
     }
 }
